e9abf8062a8f6d62fbec25f53c81c5dc6777eb69
Jason Katsaros <katsarjp@tutanota.com>
2025-04-02 14:06:17 -0400

    refactor: install utility packages

--- home.nix
+++ home.nix
@@ -8,6 +8,7 @@
     ./user/applications/media/media.nix
     ./user/applications/utilities/bitwarden.nix
     ./user/applications/utilities/yt-dlp.nix
+    ./user/applications/utilities/filezilla.nix
   ];
 
   # Home Manager needs a bit of information about you and the paths it should
