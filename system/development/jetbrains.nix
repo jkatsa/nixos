192a77e51f282f1077b82b808868440ba77448ac
Jason Katsaros <katsarjp@tutanota.com>
2025-04-04 15:39:09 -0400

    refactor: reorganize jetbrains packages

--- system/development/jetbrains.nix
+++ system/development/jetbrains.nix
@@ -1,22 +1,22 @@
 { pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [
-    jetbrains.aqua
-    jetbrains.clion
-    jetbrains.datagrip
-    jetbrains.dataspell
-    jetbrains.gateway
-    jetbrains.goland
-    jetbrains.idea-ultimate
-    jetbrains.jdk
-    jetbrains.mps
-    jetbrains.phpstorm
-    jetbrains.pycharm-professional
-    jetbrains.rider
-    jetbrains.ruby-mine
-    jetbrains.rust-rover
-    jetbrains.webstorm
-    jetbrains.writerside
+  environment.systemPackages = with pkgs.jetbrains; [
+    aqua
+    clion
+    datagrip
+    dataspell
+    gateway
+    goland
+    idea-ultimate
+    jdk
+    mps
+    phpstorm
+    pycharm-professional
+    rider
+    ruby-mine
+    rust-rover
+    webstorm
+    writerside
   ];
 }
